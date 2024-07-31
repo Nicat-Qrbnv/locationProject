3a0c79cd389c160f07fbbb421bba4d0fa1d90861
Toghrul <baghirli.togrul@gmail.com>
2024-07-31 13:06:50 +0400

    still in progress

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,17 +1,20 @@
-import { Loader } from "@googlemaps/js-api-loader";
+import { Loader } from '@googlemaps/js-api-loader';
 
 let map;
 let markers = [];
 
-const loader = new Loader({
-    apiKey: "xxxx",
-    version: "weekly",
-    libraries: ["places", "marker", "journeySharing", "streetView", "visualization"]
-});
-
 async function initMap() {
-    loader.load().then(() => {
-        map = new google.maps.Map(document.getElementById("map"), {
+    const response = await fetch('/api/maps-key');
+    const apiKey = await response.text();
+
+    const loader = new Loader({
+        apiKey: apiKey,
+        version: "weekly",
+        libraries: ["places", "marker", "journeySharing", "streetView", "visualization"]
+    });
+
+    map = await loader.load().then(() => {
+        return new google.maps.Map(document.getElementById('map'), {
             center: { lat: 40.10789164639515, lng: 46.04158226806454 },
             zoom: 14,
             mapTypeId: 'terrain',
@@ -19,30 +22,23 @@ async function initMap() {
             gestureHandling: "cooperative",
             heading: 90
         });
-
-        fetchMarkersAndDisplay();
     });
+
+    fetchMarkersAndDisplay();
 }
 
 async function fetchMarkersAndDisplay() {
-    try {
-        const response = await fetch('/api/v1/markers/all');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        clearMarkers();
-        data.forEach(marker => {
-            const mapMarker = new google.maps.Marker({
-                position: { lat: marker.latitude, lng: marker.longitude },
-                map: map,
-                title: marker.description
-            });
-            markers.push(mapMarker);
+    const response = await fetch('/api/v1/markers/all');
+    const data = await response.json();
+
+    data.forEach(marker => {
+        const mapMarker = new google.maps.Marker({
+            position: { lat: marker.latitude, lng: marker.longitude },
+            map: map,
+            title: marker.description
         });
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
+        markers.push(mapMarker);
+    });
 }
 
 function clearMarkers() {
